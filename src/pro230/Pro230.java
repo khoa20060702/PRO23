645f227b20f44c1672775b5144af2f40898efd20
BAOTRAN00531 <150698874+BAOTRAN00531@users.noreply.github.com>
2024-03-20 23:09:34 +0700

    rename com.mycompany.ui

---
+++ src/pro230/Pro230.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pro230;
+
+/**
+ *
+ * @author tran
+ */
+public class Pro230 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
